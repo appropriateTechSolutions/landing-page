9e1f7297eb870117ade9c412eb55a4698f72d854
appropriateTechSolutions <developer@appropriatetechsolutions.com>
2025-01-28 17:25:19 +0530

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        poppins: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        success: "#18c964",
+      },
+    },
+    daisyui: {
+      themes: ["light"],
+    },
+  },
+  plugins: [require("daisyui")],
+};
+export default config;
